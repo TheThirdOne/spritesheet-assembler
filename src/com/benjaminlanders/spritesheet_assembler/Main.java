6583ee3e19a2ecb8009e9d059e73c6f39f199986
Benjamin Landers <benjaminrlanders@gmail.com>
2013-04-05 21:56:22 -0700

    Made it PERFECT

--- src/com/benjaminlanders/spritesheet_assembler/Main.java
+++ src/com/benjaminlanders/spritesheet_assembler/Main.java
@@ -15,24 +15,41 @@ public class Main {
 	 */
 	public static void main(String[] args) 
 	{
-		
-		String ending = "-sprite.png";//args[0]+".png";
-		int col = 2;//Integer.parseInt(args[1]);
-		int row = 2;//Integer.parseInt(args[2]);
+		if( args.length == 0 || args[0].equalsIgnoreCase("help"))
+		{
+			System.out.println("The first argument is the ending of the files");
+			System.out.println("The second argument is the number of columns");
+			System.out.println("The third argument is the ending of rows");
+			System.out.println("type help for this help screen");
+			
+		}else
+		{
+		String ending = args[0]+".png";
+		int col = Integer.parseInt(args[1]);
+		int row = Integer.parseInt(args[2]);
 		System.out.println("Columns: "+ col+ " Rows: "+row);
 		File tempFile = new File(1+ending);
-		try {
+		try
+		{
 			BufferedImage temp = ImageIO.read(tempFile);
-			BufferedImage out = new BufferedImage(temp.getHeight()*row,temp.getWidth()*col,BufferedImage.TYPE_INT_ARGB);
+			int h = temp.getHeight();
+			int w = temp.getWidth();
+			BufferedImage out = new BufferedImage(w*col, h*row,BufferedImage.TYPE_INT_ARGB);
 			Graphics2D g = out.createGraphics();
-			g.drawImage((Image)temp, 0, 0, null);
+			for(int i = 0; i < col*row;i++)
+			{
+				tempFile = new File((i+1)+ending);
+				temp = ImageIO.read(tempFile);
+				g.drawImage((Image)temp, (i % col)*w,((int) i/col)*h, null);
+			}
 			File outputfile = new File("saved.png");
 		    ImageIO.write(out, "png", outputfile);
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		}
 		
 		
 
